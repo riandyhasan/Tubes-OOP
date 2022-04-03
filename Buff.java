66f5df63ea0662040ae696dfa080fa28acbb0d6c
riandyhasan <riandyhsn@gmail.com>
2022-04-03 07:56:51 +0700

    Create Buff.java

---
+++ Buff.java
@@ -0,0 +1,53 @@
+public class Buff{
+    private static int MIN_BUFF = -4;
+    private static int MAX_BUFF = 4;
+    private int attack, defense, specialAttack, specialDefense, speed;
+
+    public Buff(int attack, int defense, int specialAttack, int specialDefense, int speed) {
+        this.attack = attack;
+        this.defense = defense;
+        this.specialAttack = specialAttack;
+        this.specialDefense = specialDefense;
+        this.speed = speed;
+    }
+
+    public int getAttack(){
+        return this.attack;
+    }
+
+    public int getDefense(){
+        return this.defense;
+    }
+
+    public int getSpecialAttack(){
+        return this.specialAttack;
+    }
+
+    public int getSpecialDefense(){
+        return this.specialDefense;
+    }
+
+    public int getSpeed(){
+        return this.speed;
+    }
+
+    public void setAttack(int attack){
+        this.attack = attack;
+    }
+
+    public void setDefense(int defense){
+        this.defense = defense;
+    }
+
+    public void setSpecialAttack(int specialAttack){
+        this.specialAttack = specialAttack;
+    }
+
+    public void setSpecialDefense(int specialDefense){
+        this.specialDefense = specialDefense;
+    }
+
+    public void setSpeed(int speed){
+        this.speed = speed;
+    }
+}
