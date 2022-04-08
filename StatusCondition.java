f9903523ee2c67afe92d6559bce907b8a76ddb17
cinnamonboyz <91800020+cinnamonboyz@users.noreply.github.com>
2022-04-08 20:02:03 +0700

    Create StatusCondition.java

---
+++ StatusCondition.java
@@ -0,0 +1,18 @@
+public class StatusCondition extends Stats{
+    private double healthPoint, attack, defense, specialAttack, specialDefense, speed, damage;
+
+    public void burn(double healthPoint, double attack){
+        healthPoint = this.healthPoint - this.healthPoint * 1/8;
+        // yang damage nya blm ngerti
+    }
+
+    public void poison(double healthPoint){
+        healthPoint = this.healthPoint - this.healthPoint * 1/16;
+    }
+
+    // yg sleep bingungg
+
+    public void paralyze(double speed) {
+        speed = this.speed - this.speed * 1/2;
+    }
+}
