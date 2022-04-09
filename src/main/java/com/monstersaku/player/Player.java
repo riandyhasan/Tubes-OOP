57455c5f35a14b97311b544d7f69e10aee480924
riandyhasan <riandyhsn@gmail.com>
2022-04-09 14:07:33 +0700

    add player

--- src/main/java/com/monstersaku/player/Player.java
+++ src/main/java/com/monstersaku/player/Player.java
@@ -1,5 +1,41 @@
 package com.monstersaku.player;
 
+import com.monstersaku.monster.Monster;
+import java.util.ArrayList;
+
 public class Player {
-    
+    private String name;
+    private ArrayList<Monster> monsters;
+    private boolean isTurn;
+
+
+    public Player(String name, ArrayList<Monster> monsters) {
+        this.name = name;
+        this.monsters = monsters;
+        this.isTurn = true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Monster> getMonsters() {
+        return monsters;
+    }
+
+    public boolean getIsTurn(){
+        return isTurn;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addMonster(Monster monster){
+        monsters.add(monster);
+    }
+
+    public void setIsTurn(boolean isTurn){
+        this.isTurn = isTurn;
+    }
 }
